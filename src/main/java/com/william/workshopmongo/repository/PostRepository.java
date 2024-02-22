01022c3a8b6d0d3cdaf637c7f7ca6caad9544bc5
William <convidadoc38@gmai.com>
2024-02-22 11:44:37 -0300

    Consulta simples com @Query

--- src/main/java/com/william/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/william/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.william.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.william.workshopmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.william.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	//Criando uma consulta personalizada para o mongoDB por meio do spring data
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query( "{ 'title' : { $regex: ?0, $options: 'i' } }" )
+	List<Post> findByTitle(String text);
 
 }
